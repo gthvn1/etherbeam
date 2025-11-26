89d04da55a70e7cffe827a63eb0f4cb4299db79e
Guillaume <guillaume.thouvenin@vates.tech>
2025-11-26 13:25:08 +0100

    Start unix socket

---
+++ go/network/unix_socket.go
@@ -0,0 +1,9 @@
+package network
+
+import (
+	"fmt"
+)
+
+func Hello() {
+	fmt.Println("Hello from unix socket")
+}
--- go/raw_binder.go
+++ go/raw_binder.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"example.com/raw_binder/network"
 )
 
 func main() {
 	fmt.Println("Hello, Sailor!")
+	network.Hello()
 }
